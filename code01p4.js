89b09df5089b7f4c2cc75850b243dd3865448c39
Sebastian Hjorth <Hjorthensebastian@gmail.com>
2019-09-11 14:46:19 +0200

    exercise w01p4

--- code01p4.js
+++ code01p4.js
@@ -1,24 +1,32 @@
 var Renderer = {
+    rotation : 0.0,
     Run : function()
     {
         this.Setup();
-        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-        this.gl.useProgram(this.program);
+        var gl = this.gl;
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-        var colors = [vec4(1, 0, 0, 1), vec4(0, 1, 0, 1), vec4(0, 0, 1, 1)];
-        var vertices = [vec2(0.0, 0.5), vec2(-0.5, -0.5), vec2(0.5, -0.5)];
+        gl.useProgram(this.program);
 
+        var colors = [vec4(1, 0, 0, 1), vec4(0, 1, 0, 1), vec4(0, 0, 1, 1),vec4(1, 0, 0, 1)];
+        var vertices = [vec2(0.0, 0.5), vec2(0.5, 0), vec2(-0.5, 0.0), vec2(0.0, -0.5)];
+        this.vertexCount = vertices.length;
         this.SendFloatData("a_Position", vertices, 2);
         this.SendFloatData("a_Color", colors, 4);
-        this.Draw(vertices.length);
+        var obj = this;
+        window.requestAnimationFrame(function() {obj.Draw()});
     },
 
-    Draw : function(vertexCount)
+    Draw : function()
     {
         var gl = this.gl;
+        var rotationLoc = gl.getUniformLocation(this.program, "u_Rotation");
+        gl.uniform1f(rotationLoc, this.rotation);
         gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexCount);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.vertexCount);
+        this.rotation += 0.01;
+        window.requestAnimationFrame(function() {this.Draw()}.bind(this));
     },
 
     Setup : function()
