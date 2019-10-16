ad04388e2b34797d996728edc824de1bd16b04be
Sebastian Hjorth <Hjorthensebastian@gmail.com>
2019-10-16 12:29:08 +0200

    Cleanup

--- part5/code05p1.js
+++ part5/code05p1.js
@@ -40,7 +40,7 @@ var Renderer = {
 
         this.gl = setupWebGL(this.canvas);
         this.program = initShaders(this.gl, "vertex-shader", "fragment-shader");
-        this.subdivisionLevel = subdivisonSlider.value;
+
         this.material = {
             ambient : vec4(1.0, 1.0, 1.0, 1.0),
             diffuse : vec4(1.0, 1.0, 1.0, 1.0),
@@ -54,73 +54,6 @@ var Renderer = {
     {
         return this.mode.selectedIndex + 1; 
     },
-
-    DrawCube()
-    {   /*   6-----5
-         *  /|    /|
-         * 1-----0 |
-         * | 7---|-4 
-         * |/    |/
-         * 2-----3
-         */
-
-        var vertices = [
-            vec4( 1.0,  1.0,  1.0),
-            vec4(-1.0,  1.0,  1.0),
-            vec4(-1.0, -1.0,  1.0),
-            vec4( 1.0, -1.0,  1.0),
-            vec4( 1.0, -1.0, -1.0),
-            vec4( 1.0,  1.0, -1.0),
-            vec4(-1.0,  1.0, -1.0),
-            vec4(-1.0, -1.0, -1.0)
-        ];
-        
-        var colors = [
-            vec4(1.0, 0.0, 0.0, 1.0),
-            vec4(0.0, 1.0, 0.0, 1.0),
-            vec4(0.0, 0.0, 1.0, 1.0),
-            vec4(0.0, 0.0, 0.0, 1.0),
-
-            vec4(0.0, 0.0, 0.0, 1.0),
-            vec4(1.0, 0.0, 0.0, 1.0),
-            vec4(0.0, 1.0, 0.0, 1.0),
-            vec4(0.0, 0.0, 1.0, 1.0),
-        ];
-
-        var indices = [
-            1, 2, 3, // Front
-            0, 1, 3,
-
-            0, 3, 4, // Right
-            5, 0, 4,
-
-            3, 2, 7, // Bottom
-            4, 3, 7,
-            
-            2, 1, 6, // Left
-            7, 2, 6,
-
-            1, 0, 5, // Top
-            6, 0, 5,     
-            
-            4, 7, 6, // Back
-            4, 5, 6
-        ];
-
-        this.cubeBuffer = new GLBuffer(this.bufferSize, this.gl.ELEMENT_ARRAY_BUFFER, this.gl);
-
-        var offset = this.vertices.length;
-        for(i in vertices)
-        {
-            this.AddVertex(vertices[i], colors[i])
-
-        }
-
-        for(i of indices)
-        {
-            this.cubeBuffer.Add(i + offset);
-        }
-    }, 
 
     Run()
     {
@@ -144,14 +77,6 @@ var Renderer = {
         this.MVNormLocation = gl.getUniformLocation(this.program, "MVNorm")
         
         this.SetPerspective(true);
-
-        // Setup index buffers for points
-        this.pointsIndiceBuffer = new GLBuffer(this.bufferSize, gl.ELEMENT_ARRAY_BUFFER, gl); 
-
-        this.circleIndiceBuffer = new GLBuffer(this.bufferSize, gl.ELEMENT_ARRAY_BUFFER, gl);
-
-        // Setup index buffers for triangles
-        this.triangleIndiceBuffer = new GLBuffer(this.bufferSize, gl.ELEMENT_ARRAY_BUFFER, gl); 
 
         this.colors = [];
         this.colorBuffer = gl.createBuffer();
@@ -179,17 +104,9 @@ var Renderer = {
         button.addEventListener("click", this.OnClear.bind(this));
         this.canvas.addEventListener("click", this.OnClick.bind(this));
         
-        this.DrawCircle();
 
         this.dotCounter = 0;
         window.requestAnimationFrame(function() {this.Draw()}.bind(this));
-    },
-    DrawCircle()
-    {
-        this.circle.Clear()
-
-        this.circle = UnitSphere(this.subdivisionLevel, this.circle) 
-        this.circle.Commit()
     },
     OnClear : function()
     {
@@ -200,13 +117,6 @@ var Renderer = {
     {
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, this.bufferSize, this.gl.STATIC_DRAW);
-        
-        this.pointsIndiceBuffer.Clear();
-        this.triangleIndiceBuffer.Clear();
-        this.circleIndiceBuffer.Clear();
-
-        this.vertices = [];
-        this.colors = [];
     },
 
     OnClick : function()
@@ -218,92 +128,8 @@ var Renderer = {
         var centeredPos = subtract(pos, offset);
         var z = 1 - 2*((this.vertices.length + 1) / this.bufferSize);
         var normalizedPos = vec3(centeredPos[0] / (0.5*boundingRect.width), -centeredPos[1] / (0.5*boundingRect.height), z);
-        this.AddDot(normalizedPos);
     },
 
-    AddDot : function(position)
-    {
-
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
-        var vertexIndex = this.vertices.length;
-        this.gl.bufferSubData(this.gl.ARRAY_BUFFER, sizeof['vec3']*vertexIndex, flatten(position)); 
-        this.vertices.push(position);
-        
-        if(this.GetDrawMode() === 2 && (++this.dotCounter) % 3 == 0)
-        {
-            this.pointsIndiceBuffer.Strip(2);
-            this.dotCounter = 0;
-        }
-        else if(this.GetDrawMode() == 3 && (++this.dotCounter) % 2 == 0)
-        {
-            this.pointsIndiceBuffer.Strip(1);
-            this.dotCounter = 0;
-            
-            // Remove the 2nd vertex
-            this.vertices.pop();
-            var lastPosition = this.vertices[this.vertices.length - 1];
-
-            var circleWidth = Math.sqrt(Math.pow(lastPosition[0] - position[0], 2) + Math.pow(lastPosition[1] - position[1], 2));
-
-            // Render triangles
-            const triangleCount = 8;
-            var offset = (2 * Math.PI) / triangleCount;
-            var startingIndex = this.vertices.length;
-            this.circleIndiceBuffer.Add(startingIndex - 1);
-            for(var i=0;i<=triangleCount;++i)
-            {
-                var angle = offset * i;
-                this.colors.push(this.drawColor);
-                var vertexPosition = vec2(circleWidth * Math.cos(angle), circleWidth * Math.sin(angle)); 
-                vertexPosition[0] += lastPosition[0];
-                vertexPosition[1] += lastPosition[1];
-
-                this.vertices.push(vertexPosition);
-                this.circleIndiceBuffer.Add(this.vertices.length - 1);
-            }
-
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
-            this.gl.bufferSubData(this.gl.ARRAY_BUFFER, sizeof['vec3']*startingIndex, flatten(this.vertices.slice(startingIndex))); 
-
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
-            this.gl.bufferSubData(this.gl.ARRAY_BUFFER, sizeof['vec4']*startingIndex, flatten(this.colors.slice(startingIndex)));
-            return;
-        }
-        else
-        {
-            this.pointsIndiceBuffer.Add(vertexIndex);
-        }
-
-        if(this.GetDrawMode() === 2) 
-        {
-            this.triangleIndiceBuffer.Add(vertexIndex);
-        }
-
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
-        this.gl.bufferSubData(this.gl.ARRAY_BUFFER, sizeof['vec4']*this.colors.length, flatten(this.drawColor));
-        this.colors.push(this.drawColor);
-    },
-
-    AddVertex : function(position, color)
-    {
-        vertexIndex = this.vertices.length;
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
-        this.gl.bufferSubData(this.gl.ARRAY_BUFFER, sizeof['vec3']*vertexIndex, flatten([position]));
-        this.vertices.push(position);
-
-        colorIndex = this.colors.length;
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
-        this.gl.bufferSubData(this.gl.ARRAY_BUFFER, sizeof['vec4'] * colorIndex, flatten([color]));
-        this.colors.push(color);
-        return vertexIndex;
-    },
-    PrintVertices()
-    {
-        for(v of this.vertices)
-        {
-            console.log(v)
-        }
-    },
     SetPerspective : function(orth)
     {
         var mv = this.gl.getUniformLocation(this.program, "P");
@@ -415,13 +241,5 @@ perspectiveMenu.addEventListener("change", function() {
             break;
     }
 });
-
-subdivisonSlider =  document.getElementById("subdivSlider")
-subdivisonSlider.addEventListener("change", function()
-    {
-        Renderer.subdivisionLevel = Number(subdivisonSlider.value);
-        Renderer.DrawCircle()
-    }
-);
 
 onload= Renderer.Run();
